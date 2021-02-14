31c6a031c879724c12be62d5d66900ef68a1717f
Jordan Lewis <jordankareemlewis@gmail.com>
2021-02-14 03:09:25 -0500

    added profile info and navigation to app

--- app/views.py
+++ app/views.py
@@ -7,6 +7,7 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+import datetime
 
 
 ###
@@ -18,11 +19,15 @@ def home():
     """Render website's home page."""
     return render_template('home.html')
 
+@app.route('/profile')
+def profile():
+    """Render the website's profile page."""
+    return render_template('profile.html',date=format_date_joined(datetime.date(2019, 7, 13)))
 
 @app.route('/about/')
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    return render_template('about.html', name="Jordan Lewis")
 
 
 ###
@@ -52,6 +57,11 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
+
+def format_date_joined(date_joined):
+    return "Joined " + date_joined.strftime("%B, %Y")
+
 
 
 if __name__ == '__main__':
